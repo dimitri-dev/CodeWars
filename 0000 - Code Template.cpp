91630081b60c4a8b353f11e5d92630dde3fdb20b
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-11-26 15:57:56 +0100

    Code Template
    This is the code template for every solution on here (so it is easier to code).

---
+++ 0000 - Code Template.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+func funcName(funcParam)
+{
+
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+}
