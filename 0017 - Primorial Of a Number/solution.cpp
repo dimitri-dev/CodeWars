a3c42dffcf9da862aa753811fe5a27b2dd8c0a65
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-11-27 18:20:05 +0100

    Challenge 17 - solved
    Challenge link: https://www.codewars.com/kata/5a99a03e4a6b34bb3c000124/

---
+++ 0017 - Primorial Of a Number/solution.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+bool isPrime(int num) {
+    for (int i = 2; i <= sqrt(num); i++) if (num % i == 0) return false;
+    return num <= 1 ? false : true;
+}
+
+unsigned long long numPrimorial(unsigned short int number)
+{
+    int primeCounter = 0;
+    int i = 2;
+    unsigned long long resultant = 1;
+    while (primeCounter != number)
+    {
+        if (!isPrime(i))
+            ++i;
+        else {
+            resultant *= i;
+            ++i;
+            ++primeCounter;
+        }
+    }
+    return resultant;
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    cout << numPrimorial(9) << endl;
+}
