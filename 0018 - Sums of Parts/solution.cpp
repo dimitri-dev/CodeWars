6d14864a4dc326c043cd0c431bb1164bd45bed05
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-11-27 18:35:53 +0100

    Challenge 18 - solved
    Challenge link: https://www.codewars.com/kata/5ce399e0047a45001c853c2b/

---
+++ 0018 - Sums of Parts/solution.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+std::vector<unsigned long long> partsSum(std::vector<unsigned long long> ls) {
+    std::vector<unsigned long long> resultant;
+    unsigned long long resMain = 0;
+
+    for (auto k : ls)
+        resMain += k;
+
+    for (size_t i = 0; i < ls.size(); ++i) {
+        resultant.push_back(resMain);
+        resMain -= ls[i];
+    }
+
+    resultant.push_back(0);
+    return resultant;
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    std::vector<unsigned long long> res = partsSum({ 0, 1, 3, 6, 10 });
+    for (auto ck : res)
+        cout << ck << " ";
+}
