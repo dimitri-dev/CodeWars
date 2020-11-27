1f9afa9047a57086af04a26f1d2a3fba9411e6ef
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-11-27 19:17:20 +0100

    Challenge 19 - solved
    Challenge link: https://www.codewars.com/kata/550554fd08b86f84fe000a58/

---
+++ 0019 - Which are in/solution.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+static std::vector<std::string> inArray(std::vector<std::string> array1, std::vector<std::string> array2) {
+    std::vector<std::string> result;
+
+    for (auto x : array2)
+        for (auto y : array1)
+            if (x.find(y) != std::string::npos) {
+                if (std::count(result.begin(), result.end(), y) == 0)
+                    result.push_back(y);
+            }
+                
+
+    std::sort(result.begin(), result.end());
+    return result;
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    std::vector<std::string> arr1 = { "arp", "live", "strong" };
+    std::vector<std::string> arr2 = { "lively", "alive", "harp", "sharp", "armstrong" };
+
+    std::vector<std::string> sol = inArray(arr1, arr2);
+    for (auto x : sol)
+        cout << x << endl;
+}
