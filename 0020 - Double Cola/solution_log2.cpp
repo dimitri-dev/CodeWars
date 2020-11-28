caee063928828eac6e8a5426242b733f4bdbdeb1
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-11-28 19:39:41 +0100

    Challenge 20 - solved (2)
    Second solution - arguably better
    
    Challenge link: https://www.codewars.com/kata/551dd1f424b7a4cdae0001f0/

---
+++ 0020 - Double Cola/solution_log2.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+std::string who_is_next2(std::vector<std::string> v, long long n) {
+    long long l = pow(2, floor(log2(ceil((double)n-- / v.size()))));
+    return v[(n - (l - 1) * v.size()) / l];
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    std::vector<std::string> names1 = { "Sheldon", "Leonard", "Penny", "Rajesh", "Howard" };
+    std::vector<std::string> names2 = { "Sheldon", "Leonard"};
+    std::vector<std::string> names3 = { "Sheldon", "Leonard", "Penny", "Rajesh", "Howard", "Daisuke", "Motoko"};
+
+    cout << who_is_next(names1, 7) << endl;
+    /// Leonard
+
+    cout << who_is_next(names2, 7) << endl;
+    /// Sheldon
+
+    cout << who_is_next(names3, 9) << endl;
+    /// Sheldon
+
+    cout << who_is_next(names1, 1) << endl;
+    /// Sheldon
+
+    cout << who_is_next(names1, 52) << endl;
+    /// Penny
+
+    cout << who_is_next(names1, 1) << endl;
+    /// Sheldon
+
+    cout << who_is_next(names1, 10010) << endl;
+    /// Howard
+
+    cout << who_is_next(names1, 7230702951) << endl;
+    // Leonard
+}
