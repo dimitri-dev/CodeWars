8f3d5f5eb2f52b010795d71497a8289e4b3b9e1d
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-11-29 18:49:05 +0100

    Challenge 22 - solved
    Challenge link: https://www.codewars.com/kata/5511b2f550906349a70004e1/

---
+++ 0022 - Last digit of a large number/solution.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int last_digit(std::string str1, std::string str2) {
+    if (str1.length() == 1 && str1[0] == '0' &&
+        str2.length() == 1 && str2[0] == '0')
+        return 1;
+
+    if (str2.length() == 1 && str2[0] == '0')
+        return 1;
+
+    if (str1.length() == 1 && str1[0] == '0')
+        return 0;
+
+    int last_digit_a = (int)str1[str1.length() - 1] - '0';
+
+    if (last_digit_a == 0) return 0;
+    if (last_digit_a == 1) return 1;
+    else if (last_digit_a == 5) return 5;
+    else if (last_digit_a == 6) return 6;
+
+    int lastTwo_b;
+
+    if (str2.length() >= 2)
+        lastTwo_b = ((int)str2[str2.length() - 1] - '0') + (((int)str2[str2.length() - 2] - '0') * 10);
+    else
+        lastTwo_b = ((int)str2[str2.length() - 1] - '0');
+
+    return ((int)pow(last_digit_a, (lastTwo_b % 4 == 0) ? 4 : (lastTwo_b % 4)) % 10);
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    cout << last_digit("97", "1");
+}
