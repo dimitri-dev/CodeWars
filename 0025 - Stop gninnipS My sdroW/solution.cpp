9e7118ed5ca33fbfdcd35f4144bdec1654e61fd4
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-11-29 21:51:18 +0100

    Challenge 25 - solved
    Challenge link: https://www.codewars.com/kata/5264d2b162488dc400000001/

---
+++ 0025 - Stop gninnipS My sdroW/solution.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <sstream>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+std::string spinWords(std::string str) {
+    std::istringstream iss(str);
+    std::vector<std::string> splitString((std::istream_iterator<std::string>(iss)), std::istream_iterator<std::string>());
+
+    std::string result = "";
+
+    for (auto s : splitString)
+    {
+        if (s.length() >= 5)
+            std::reverse(s.begin(), s.end());
+
+        result += s + " ";
+    }
+    result.erase(result.end() - 1);
+    return result;
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    cout << spinWords("This is a stupid example") << endl;
+}
