487fdcbf496f1e028c125790ec8a4cdd6d18c6d1
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-11-30 00:22:24 +0100

    Challenge 26 - solution
    Challenge link: https://www.codewars.com/kata/5a946d9fba1bb5135100007c/

---
+++ 0026 - Transform To Prime/solution.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+
+#include <numeric>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+bool isPrime(int num) {
+    for (int i = 2; i <= sqrt(num); i++) if (num % i == 0) return false;
+    return num <= 1 ? false : true;
+}
+
+int minimumNumber(std::vector<int> numbers)
+{
+    long long elemSum = std::accumulate(numbers.begin(), numbers.end(), 0);
+    std::pair<int, bool> goUp = std::make_pair(1, false);
+
+    if (isPrime(elemSum))
+        return 0;
+
+    while (goUp.second != true)
+    {
+        if (isPrime(elemSum + goUp.first)) {
+            goUp.second = true;
+            break;
+        }
+
+        ++goUp.first;
+    }
+
+    return goUp.first;
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    cout << minimumNumber({ 2,12,8,4,6 }) << endl;
+    cout << minimumNumber({ 50,39,49,6,17,28 }) << endl;
+}
