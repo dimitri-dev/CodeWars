db9133474f52f04ffbf92ecacaf21436655b7efe
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-11-30 01:09:50 +0100

    Challenge 28 - solved
    Challenge link: https://www.codewars.com/kata/5a87449ab1710171300000fd/

---
+++ 0028 - Tidy Number Special Numbers Series 9/solution.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+
+#include <numeric>
+#include <algorithm>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+bool tidyNumber(int number)
+{
+    std::string n = std::to_string(number);
+    char lastC = '0';
+    for (auto c : n) {
+        if (c < lastC) return false;
+        else lastC = c;
+    }
+    return true;
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    cout << tidyNumber(1024) << endl;
+}
