42c051b1b4ff0f3770219d1c899b39bb8f800867
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-11-30 10:28:27 +0100

    Challenge 29 - solved
    Challenge link: https://www.codewars.com/kata/5a04133e32b8b998dc000089

---
+++ 0029 - Dominant array elements/solution.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+
+#include <numeric>
+#include <algorithm>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+std::vector<int> solve(std::vector<int> array) {
+    std::vector<int> better;
+    for (int i = 0; i < array.size(); ++i)
+        if (array[i] >= *std::max_element(array.begin() + i, array.end()))
+            if (std::count(better.begin(), better.end(), array[i]) == 0)
+                better.push_back(array[i]);
+
+    return better;
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    auto x = solve({ 92,52,93,31,89,87,77,105 });
+    for (auto k : x)
+        cout << k << endl;
+}
