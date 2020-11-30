ad48eb15b188863e28ea5e9ffa32af811aa54967
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-12-01 00:51:33 +0100

    Challenge 31 - solved
    Challenge link: https://www.codewars.com/kata/541c8630095125aba6000c00/

---
+++ 0031 - Sum of Digits - Digital Root/solution.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+
+#include <numeric>
+#include <algorithm>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int digital_root(int n)
+{
+    std::string s = std::to_string(n);
+    long long sumDig = 0;
+    while (s.length() != 1) {
+        sumDig = std::accumulate(s.begin(), s.end(), 0);
+        s = std::to_string((sumDig - s.length() * '0'));
+    }
+    return (std::accumulate(s.begin(), s.end(), 0) - s.length() * '0');
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    cout << digital_root(167346) << endl;
+}
