16517848246793b38ddd96d8f1d9c18c2130a5c9
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-12-01 08:41:36 +0100

    Challenge 32 - solved
    Challenge link: https://www.codewars.com/kata/514b92a657cdc65150000006/

---
+++ 0032 - Multiples of 3 or 5/solution.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+
+#include <numeric>
+#include <algorithm>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int solution(int n) {
+    return ((3 * ((n - 1) / 3) * ((n - 1) / 3 + 1) / 2) + (5 * ((n - 1) / 5) * ((n - 1) / 5 + 1) / 2) - (15 * ((n - 1) / 15) * ((n - 1) / 15 + 1) / 2));
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    cout << solution(10) << endl;
+}
