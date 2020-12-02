75df1a52df7305a3d712b8e3e76eb69d8368cb56
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-12-02 02:00:28 +0100

    Challenge 37 - solved
    Challenge link: https://www.codewars.com/kata/5544c7a5cb454edb3c000047/

---
+++ 0037 - Bouncing balls/solution.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+class Bouncingball
+{
+public:
+    static int bouncingBall(double h, double bounce, double window)
+    {
+        if (h <= 0 || bounce <= 0 || bounce >= 1 || window >= h)
+            return -1;
+        
+        int c = 1;
+        double curr = h * bounce;
+        while (curr > window) {
+            ++++c;
+            curr *= bounce;
+        }
+        return c;
+    }
+};
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    cout << Bouncingball::bouncingBall(3, 0.66, 1.5) << endl;
+    cout << Bouncingball::bouncingBall(30, 0.66, 1.5) << endl;
+}
