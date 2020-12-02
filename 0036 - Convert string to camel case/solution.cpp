9017e7fceb19c69be7e3192f3d1d943e13559253
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-12-02 01:47:36 +0100

    Challenge 36 - solved
    Challenge link: https://www.codewars.com/kata/517abf86da9663f1d2000003/

---
+++ 0036 - Convert string to camel case/solution.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+std::string to_camel_case(std::string text) {
+    for (std::string::iterator i = text.begin(); i < text.end(); ++i){
+        if (*i == '_' || *i == '-') {
+            text.erase(i);
+            *(i) = std::toupper(*(i));
+        }
+    }
+    return text;
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    cout << to_camel_case("the_stealth_warrior");
+}
