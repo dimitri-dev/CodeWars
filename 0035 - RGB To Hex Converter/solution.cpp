826f7e67899f4d4a9da4980a521cfcbe317c3bef
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-12-02 01:40:53 +0100

    Challenge 35 - solved
    Challenge link: https://www.codewars.com/kata/513e08acc600c94f01000001/

---
+++ 0035 - RGB To Hex Converter/solution.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+class RGBToHex
+{
+public:
+    static std::string rgb(int r, int g, int b)
+    {
+        std::string sol = "";
+
+        if (r < 0) r = 0;
+        else if (r > 255) r = 255;
+
+        if (g < 0) g = 0;
+        else if (g > 255) g = 255;
+
+        if (b < 0) b = 0;
+        else if (b > 255) b = 255;
+
+        std::stringstream a;
+        a << std::setfill('0') << std::setw(2) << std::hex << r;
+        a << std::setfill('0') << std::setw(2) << std::hex << g;
+        a << std::setfill('0') << std::setw(2) << std::hex << b;
+        a >> sol;
+
+        std::transform(sol.begin(), sol.end(), sol.begin(), ::toupper);
+        return sol;
+    }
+};
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    cout << RGBToHex::rgb(0, 0, 0) << endl;
+    cout << RGBToHex::rgb(1, 2, 3) << endl;
+    cout << RGBToHex::rgb(255, 255, 255) << endl;
+    cout << RGBToHex::rgb(254, 253, 252) << endl;
+    cout << RGBToHex::rgb(-20, 253, 125) << endl;
+}
