2d05a95dc8ca304ee9c4f9f15c888a0404a44c81
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-12-05 19:49:09 +0100

    Challenge 43 - solved
    Challenge link: https://www.codewars.com/kata/515de9ae9dcfc28eb6000001/

---
+++ 0043 - Split Strings/solution.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+
+#include <string>
+#include <sstream>
+#include <numeric>
+#include <algorithm>
+#include <map>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+std::vector<std::string> solution(const std::string& a) {
+    std::string s = a;
+
+    if (s.length() % 2 == 1) s += "_";
+    std::vector<std::string> res;
+
+    for (size_t i = 0; i < s.length(); ++++i)
+        res.push_back(s.substr(i, 2));
+
+    return res;
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    auto res = solution("abcde");
+    for (auto x : res)
+        cout << x << endl;
+}
